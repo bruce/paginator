45505c1504ae5bea0a50c7d4c9353a348212160b
Bruce Williams <bruce@codefluency.com>
2006-10-24 15:09:22 +0000

    Initial import (1.0.1)

---
+++ pkg/paginator-1.0.1/lib/paginator.rb
@@ -0,0 +1,108 @@
+require 'forwardable'
+
+class Paginator
+  
+  VERSION = '1.0.1'
+
+  class ArgumentError < ::ArgumentError; end
+  class MissingCountError < ArgumentError; end
+  class MissingSelectError < ArgumentError; end  
+  
+  attr_reader :per_page  
+  
+  # Instantiate a new Paginator object
+  #
+  # Provide:
+  # * A total count of the number of objects to paginate
+  # * The number of objects in each page
+  # * A block that returns the array of items
+  #   * The block is passed the item offset 
+  #     (and the number of items to show per page, for
+  #     convenience)
+  def initialize(count, per_page, &select)
+    @count, @per_page = count, per_page
+    unless select
+      raise MissingSelectError, "Must provide block to select data for each page"
+    end
+    @select = select
+  end
+  
+  # Total number of pages
+  def number_of_pages
+    (@count / @per_page) + (@count % @per_page > 0 ? 1 : 0)
+  end
+  
+  # First page object
+  def first
+    page 1
+  end
+  
+  # Last page object
+  def last
+    page number_of_pages
+  end
+  
+  # Iterate through pages
+  def each
+    1.upto(number_of_pages) do |number|
+      yield page(number)
+    end
+  end
+  
+  # Retrieve page object by number
+  def page(number)
+    number = (n = number.to_i) > 0 ? n : 1
+    Page.new(self, number, lambda{ @select.call((number - 1) * @per_page, @per_page) })
+  end
+  
+  # Page object
+  #
+  # Retrieves items for a page and provides metadata about the position
+  # of the page in the paginator
+  class Page
+    
+    extend Forwardable
+    def_delegator :@pager, :first, :first
+    def_delegator :@pager, :last, :last
+    def_delegator :items, :each
+        
+    attr_reader :number, :pager
+    
+    def initialize(pager, number, select) #:nodoc:
+      @pager, @number = pager, number
+      @select = select
+    end
+    
+    # Retrieve the items for this page
+    # * Caches
+    def items
+      @items ||= @select.call
+    end
+    
+    # Checks to see if there's a page before this one
+    def prev?
+      @number > 1
+    end
+    
+    # Get previous page (if possible)
+    def prev
+      @pager.page(@number - 1) if prev?
+    end
+    
+    # Checks to see if there's a page after this one
+    def next?
+      @number < @pager.number_of_pages
+    end
+    
+    # Get next page (if possible)
+    def next
+      @pager.page(@number + 1) if next?
+    end
+    
+    def ==(other) #:nodoc:
+      @pager == other.pager && self.number == other.number
+    end
+    
+  end
+  
+end
---
+++ pkg/paginator-1.0.1/test/test_paginator.rb
@@ -0,0 +1,76 @@
+require 'test/unit'
+require 'paginator'
+
+class PaginatorTest < Test::Unit::TestCase
+
+  PER_PAGE = 10
+
+  def setup
+    @data = (0..43).to_a
+    @pager = Paginator.new(@data.size, PER_PAGE) do |offset,per_page|
+      @data[offset,per_page]
+    end
+  end
+  
+  def test_initializing_paginator_raises_exception
+    assert_raises(Paginator::MissingSelectError) do
+      @pager = Paginator.new(@data.size, PER_PAGE)
+    end
+  end  
+  
+  def test_can_get_last_page_from_page_object
+    assert_equal @pager.last, @pager.page(2).last
+  end
+  
+  def test_can_get_first_page_from_page_object
+    assert_equal @pager.first, @pager.page(2).first    
+  end
+
+  def test_does_not_exceed_per_page
+    @pager.each do |page|
+      assert page.items.size <= PER_PAGE
+    end
+  end
+  
+  def test_only_last_page_has_less_items
+    @pager.each do |page|
+      if page != @pager.last
+        assert page.items.size <= PER_PAGE
+      else
+        assert page.items.size < PER_PAGE
+      end
+    end    
+  end
+  
+  def test_returns_correct_first_page
+    assert_equal @pager.page(1).number, @pager.first.number
+  end
+  
+  def test_returns_correct_last_page
+    assert_equal @pager.page(5).number, @pager.last.number
+  end
+  
+  def test_last_page_has_no_next_page
+    assert !@pager.last.next?
+    assert !@pager.last.next
+  end
+
+  def test_first_page_has_no_prev_page
+    assert !@pager.first.prev?
+    assert !@pager.first.prev
+  end
+  
+  def test_each
+    @pager.each do |page|
+      assert page
+      page.each do |item|
+        assert item
+      end
+    end
+  end
+  
+  def test_number_of_pages
+    assert_equal 5, @pager.number_of_pages
+  end
+
+end
